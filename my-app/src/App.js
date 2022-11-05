5ebac7f0eae02d55d324aadb08c2ec3077ab6b82
ViniMacedo <74210802+ViniMacedo@users.noreply.github.com>
2022-11-05 14:49:18 -0300

    mudança app js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import axios from "axios";
 
 class App extends React.Component {
-  render() {
+ state = {
+     dados: []
+ }
+
+  componentDidMount() {
+      axios.get("http://localhost:6123/")
+          .then((response)=> {
+              const dados = response.data;
+              this.setState({dados});
+          })
+  }
+
+    render() {
     return (
       <Plot
         data={[
           {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
+            // x: [1, 2, 3],
+            // y: [2, 3, 6],
             type: 'scatter',
             mode: 'lines+markers',
             marker: {color: 'blue'},
           },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+          {type: 'bar', x: this.state.dados, y: this.state.dados}, //todo: definir o X e o Y
         ]}
-        layout={ {width: 800, height: 600, title: 'A Fancy Plot'} }
+        layout={ {width: 800, height: 600, title: 'Planta Data',justifyContent:"center"} }
       />
     );
   }
